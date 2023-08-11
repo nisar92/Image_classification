aa3ef4ca218061aa9b037ca54a937c088fbf3aee
nisar92 <89431669+nisar92@users.noreply.github.com>
2023-08-11 09:43:05 +0530

    First Commit

---
+++ init_setup.sh
@@ -0,0 +1,6 @@
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda env with python 3.11"
+conda create --prefix ./env python=3.11 -y
+echo [$(date)]: "activate env"
+source activate ./env
+echo [$(date)]: "END"
