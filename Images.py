2eebdee74975994573319a4f9ba7e9187416437a
nisar92 <89431669+nisar92@users.noreply.github.com>
2023-09-11 08:24:31 +0530

    finalized

--- Images.py
+++ Images.py
@@ -12,57 +12,55 @@ import requests
 import io
 from PIL import Image
 
-def get_urls(query:str,delay:int)->set:
-    images_urls = set()
-    browser=webdriver.Chrome()
+def get_urls(query: str, delay: int) ->set:
+    images_urls= set()
+    browser= webdriver.Chrome()
     browser.get('https://images.google.com/')
-    search_box=browser.find_element(By.CSS_SELECTOR,'#APjFqb.gLFyf')
+    search_box= browser.find_element(By.CSS_SELECTOR, '#APjFqb.gLFyf')
     search_box.send_keys(query)
     search_box.submit()
-    
+
     time.sleep(delay)
     browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     time.sleep(delay)
     browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     time.sleep(delay)
     browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
-    thumbnails = browser.find_elements(By.CLASS_NAME,"Q4LuWd")
+    thumbnails = browser.find_elements(By.CLASS_NAME, "Q4LuWd")
     for image in thumbnails:
         if image.get_attribute('src'):
-            images_urls.add(image.get_attribute('src'))
+           images_urls.add(image.get_attribute('src'))
+
     return images_urls
-
-
-
 
 '''
 def setup_browser() -> webdriver.Chrome:
-    broswer=webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    broswer.get('https://images.google.com/')
+    browser= webdriver.Chrome(service= ChromeService(ChromeDriverManager().install()))
+    browser.get('https://images.google.com/')
     time.sleep(2)
 
-    return broswer
+    return browser
 '''
 
-def download_images(query,delay,no_of_images):
-    images_urls=get_urls(query,delay)
+def download_images(query, delay, no_of_images):
+    image_urls= get_urls(query, delay)
     try:
-        if images_urls is not None:
-            os.makedirs(query,exist_ok=True)
-            for i,url in enumerate(images_urls):
+        if image_urls is not None:
+            os.makedirs(query, exist_ok=True)
+            for i, url in enumerate(image_urls):
                 if i==no_of_images:
-                    break
-                image_content = requests.get(url).content
-                image_file = io.BytesIO(image_content)
+                   break
+                image_content= requests.get(url).content
+                image_file=  io.BytesIO(image_content)
                 image= Image.open(image_file)
-                file_path=os.path.join(os.path.curdir,query,str(i)+".jpg")
-
-                with open(file_path,'wb') as f:
-                    image.save(f,"JPEG")
+                file_path= os.path.join(os.path.curdir, query, str(i)+ ".jpg")
+                
+                with open(file_path, 'wb') as f:
+                   image.save(f, "JPEG")
             return 'images download successfully'
         else:
-            return "No Images Found"
+            return "No Images found"
     except Exception as e:
         return e
 
-download_images('Narendra modi',0,15)
+download_images('modi', 2,100)
